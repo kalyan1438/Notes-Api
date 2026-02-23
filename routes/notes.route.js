3233814b07ac27ccd915c8129417468d14ff706b
Kalyan Rao <kalyanrao135@gmail.com>
2026-02-23 19:06:27 +0530

    day12

--- routes/notes.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import * as ctlr from "../controllers/notes.controller.js"
-
-const router = express.Router();
-router.post("/",ctlr.createNote);
-router.get("/",ctlr.readAll);
-export default router;
