ae0baddb9fd5b53ef3d8ba7f87ef72fd1d69d90d
Kalyan Rao <kalyanrao135@gmail.com>
2026-02-26 21:55:14 +0530

    module

--- app.js
+++ app.js
@@ -1,33 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
-import rateLimit from "express-rate-limit";
-import xss from "xss-clean"
-dotenv.config();
-
 const app = express();
-
-app.use(cors(),{
-    origin:"http://localhost:3000",
-    method:["GET","POST"],
-    credentials:true
-});
-app.use(helmet());
-app.use(rateLimit({
-    windowMs: 15*60*1000,
-    max:100,
-    message:"to mant request, try again later",
-}))
-app.use(xss())
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "API Running..." });
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.get("/notes/:id",(req,res)=>{
+    res.json({
+        userId:req.params.id,
+        title:"Task 1",
+        content:"21 to complete the Backend ",
+        Timestamp:true,
+    })
+})
+app.listen(3000);
