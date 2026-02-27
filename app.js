269cb545bad928ab22a569b6b257706ccaf9a10e
Kalyan Rao <kalyanrao135@gmail.com>
2026-02-27 21:11:43 +0530

    db added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import connectDB from "./config/db";
 const app = express();
+connectDB();
 app.use(morgan("dev"));
 app.get("/Notes",(req,res)=>{
     res.send("Notes Api");
